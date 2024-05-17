918d525083d2b79948c81a2b400b90723a31237c
Makar <147124109+Refourster@users.noreply.github.com>
2024-05-17 12:16:44 +0500

    cors update

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,5 @@
 const allowedCors = [
-  "https://practicum.yandex.ru",
-  "https://students-projects.ru",
-  "http://localhost:3000",
-  "http://localhost:3001",
+  "fronpe.nomoredomainswork.ru"
 ];
 
 function cors(req, res, next) {
@@ -11,8 +8,9 @@ function cors(req, res, next) {
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
 
-module.exports = cors;
+module.exports = cors; 
