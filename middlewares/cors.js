4dc8fe9a710eeebbcba1e1b71d6835a5d0bde840
Makar <147124109+Refourster@users.noreply.github.com>
2024-05-17 13:09:26 +0500

    end

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = "https://fronpe.nomoredomainswork.ru";
+const allowedCors = ["https://fronpe.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
